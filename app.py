8607727a79f8d0ee5bc05d8c2294cb8f7a631c94
arjunahmads20 <arjunahmads20@gmail.com>
2025-12-24 14:30:25 +0700

    Change hello_world to return 'Hello world'

--- app.py
+++ app.py
@@ -4,5 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return "Hello world"
+    # return render_template("index.html")
     # return "<p>Hello, World!</p>"
